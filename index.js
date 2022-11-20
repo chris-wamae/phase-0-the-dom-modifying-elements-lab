562553708a27b191bca55312f2fed4f598f64cb4
chris-wamae <chriswamae123@gmail.com>
2022-11-20 21:36:13 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.getElementById("main")
+main.remove()
+
+const newHeader = document.createElement("h1")
+document.body.append(newHeader)
+
+newHeader.setAttribute("id","victory")
+
+newHeader.textContent = "Chris is the champion"
